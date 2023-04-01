3c43eaf492bffd1889147be7b4045db443b6377f
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-04-01 23:46:57 +0200

    Update remnote-install.sh

--- opt/remnote-install.sh
+++ opt/remnote-install.sh
@@ -9,15 +9,16 @@ mkdir -p /usr/share/applications
 cd Downloads
 
 # Download Remnote AppImage and rename it
-curl -L -o Remnote.AppImage https://www.remnote.com/desktop/linux
-
-# Download Remnote Icon
+curl -L -o remnote.AppImage https://www.remnote.com/desktop/linux/latest
 
 # Make the AppImage executable
-chmod +x Remnote.AppImage
+chmod +x remnote.AppImage
 
 # Move the AppImage to /opt
-sudo mv Remnote.AppImage /opt/Remnote
+sudo mv remnote.AppImage /opt
+
+# Download Remnote icon and move it to /usr/share/icons/hicolor/512x512/apps
+sudo wget -O /usr/share/icons/hicolor/512x512/apps/remnote.png https://raw.githubusercontent.com/d3ltaaa/arch-packages/main/opt/remnote-icon.png
 
 # Create a desktop file for the Remnote app
 sudo tee /usr/share/applications/remnote.desktop > /dev/null <<EOF
@@ -28,7 +29,7 @@ Exec=/opt/Remnote.AppImage
 Icon=remnote
 Terminal=false
 Type=Application
-Categories=Utility;TextEditor;
+Categories=Education;
 EOF
 
 # Make the desktop file executable
@@ -36,3 +37,4 @@ sudo chmod +x /usr/share/applications/remnote.desktop
 
 # Update your application list
 sudo update-desktop-database
+
