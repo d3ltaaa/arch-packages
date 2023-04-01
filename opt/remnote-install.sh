e8060f5af303ee613c0784aeec9488b06e9a04b8
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-04-01 23:58:14 +0200

    Update remnote-install.sh

--- opt/remnote-install.sh
+++ opt/remnote-install.sh
@@ -9,13 +9,13 @@ mkdir -p /usr/share/applications
 cd Downloads
 
 # Download Remnote AppImage and rename it
-curl -L -o remnote.AppImage https://www.remnote.com/desktop/linux/latest
+curl -L -o remnote.AppImage https://www.remnote.com/desktop/linux
 
 # Make the AppImage executable
 chmod +x remnote.AppImage
 
 # Move the AppImage to /opt
-sudo mv remnote.AppImage /opt
+sudo mv remnote.AppImage /opt/Remnote
 
 # Download Remnote icon and move it to /usr/share/icons/hicolor/512x512/apps
 sudo wget -O /usr/share/icons/hicolor/512x512/apps/remnote.png https://raw.githubusercontent.com/d3ltaaa/arch-packages/main/opt/remnote-icon.png
@@ -25,8 +25,8 @@ sudo tee /usr/share/applications/remnote.desktop > /dev/null <<EOF
 [Desktop Entry]
 Name=Remnote
 Comment=Note-taking and spaced repetition software
-Exec=/opt/Remnote.AppImage
-Icon=remnote
+Exec=/opt/Remnote/remnote.AppImage
+Icon=/usr/share/icons/hicolor/512x512/apps/remnote.png
 Terminal=false
 Type=Application
 Categories=Education;
